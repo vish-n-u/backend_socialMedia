6b6eec5325311c7eff915f8ee23538fc0b107d6b
vish-n-u <vishnuna26@gmail.com>
2023-05-10 10:21:52 +0530

    added env file

--- config/server.config.js
+++ config/server.config.js
@@ -4,4 +4,4 @@ port = process.env.PORT || 3000;
 Origin = process.env.ORIGIN;
 secretKey = process.env.SECRET_KEY;
 refreshKey = process.env.REFRESH_SECRET_KEY;
-module.exports = { secretKey, port, refreshKey };
+module.exports = { secretKey, port, refreshKey, Origin };
