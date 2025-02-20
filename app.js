c731cb8ed1064f65cf178737524cddffc684f438
vish-n-u <100856840+vish-n-u@users.noreply.github.com>
2025-02-20 22:54:46 +0530

    minor change

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path")
 const bodyParser = require("body-parser");
 
 const DB_URL = require("./config/db.config");
